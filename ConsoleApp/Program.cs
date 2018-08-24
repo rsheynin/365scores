581e37c02d9ddfb35d078b9902207069e1e7f86e
Roman Sheynin <rsheynin@gmail.com>
2018-08-24 12:36:59 +0300

    ConsoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"http://{HttpContext.Request.Host}/page.html");
+
+            Stream stream = await result.Content.ReadAsStreamAsync();
+
+            HtmlDocument doc = new HtmlDocument();
+
+            doc.Load(stream);
+
+            HtmlNodeCollection links = doc.DocumentNode.SelectNodes("//a[@href]");//the parameter is use xpath see: https://www.w3schools.com/xml/xml_xpath.asp 
+
+            return View(links);
+        }
+    }
+}
